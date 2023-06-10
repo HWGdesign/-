fb9692ae97d46dbeed4f31c4cfc18f6292cdd980
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-06-10 21:01:11 +0200

    Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className=" flex w-full h-screen">
+      <div className="flex w-full h-screen items-center justify-center lg:w-1/2">
+        <div className="  bg-sky-200 h-4/5 w-4/5 rounded-xl p-10">
+          <div className=" flex items-center justify-center outline-1 outline-dashed h-full w-full outline-slate-100 outline-2 rounded-md">
+            <div> Login & Password & Socials </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 hidden lg:flex h-full w-full bg-slate-200 items-center justify-center ">
+        <div className=" w-60 h-60 flex items-center justify-center">
+          Some img
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/Components/Form.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio,
+        corporis!
+      </p>
+      <div>
+        <div>
+          <label>Email</label>
+          <input className="" placeholder="Email" />
+        </div>
+      </div>
+    </div>
+  );
+}
